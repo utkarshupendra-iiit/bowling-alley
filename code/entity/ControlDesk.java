935a48eb51e3bb123fb3d5920c43d3c9802143eb
aditya1515 <aditya.khandelwal@research.iiit.ac.in>
2021-02-12 23:16:10 +0530

    removed some unecessary comments

--- code/entity/ControlDesk.java
+++ code/entity/ControlDesk.java
@@ -1,44 +1,4 @@
-package entity;/* entity.ControlDesk.java
- *
- *  Version:
- *  		$Id$
- * 
- *  Revisions:
- * 		$Log: entity.ControlDesk.java,v $
- * 		Revision 1.13  2003/02/02 23:26:32  ???
- * 		entity.ControlDesk now runs its own thread and polls for free lanes to assign queue members to
- * 		
- * 		Revision 1.12  2003/02/02 20:46:13  ???
- * 		Added " 's entity.Party" to party names.
- * 		
- * 		Revision 1.11  2003/02/02 20:43:25  ???
- * 		misc cleanup
- * 		
- * 		Revision 1.10  2003/02/02 17:49:10  ???
- * 		Fixed problem in getPartyQueue that was returning the first element as every element.
- * 		
- * 		Revision 1.9  2003/02/02 17:39:48  ???
- * 		Added accessor for lanes.
- * 		
- * 		Revision 1.8  2003/02/02 16:53:59  ???
- * 		Updated comments to match javadoc format.
- * 		
- * 		Revision 1.7  2003/02/02 16:29:52  ???
- * 		Added events.ControlDeskEvent and observer.ControlDeskObserver. Updated entity.Queue to allow access to Vector so that contents could be viewed without destroying. Implemented observer model for most of entity.ControlDesk.
- * 		
- * 		Revision 1.6  2003/02/02 06:09:39  ???
- * 		Updated many classes to support the view.ControlDeskView.
- * 		
- * 		Revision 1.5  2003/01/26 23:16:10  ???
- * 		Improved thread handeling in lane/controldesk
- * 		
- * 
- */
-
-/**
- * Class that represents control desk
- *
- */
+package entity;
 
 import observer.ControlDeskObserver;
 import persistence.BowlerFile;
